0f08860ccd058bec1400c2345cdff2717aa45400
Gabriela Guida <61994595+gabsguida@users.noreply.github.com>
2020-06-15 12:51:57 -0300

    uso de mock no fetch - api está quebrada

--- src/pages/Home/fetchProducts.js
+++ src/pages/Home/fetchProducts.js
@@ -1,8 +1,11 @@
 import {  actionProductsLoadComplete, actionProductsLoadError } from './actions';
 import store from '../../store';
 
+
+//API -> https://5e9935925eabe7001681c856.mockapi.io/api/v1/catalog
+// use of mocks because of broken API
 export function fetchProducts() {
-    fetch("https://5e9935925eabe7001681c856.mockapi.io/api/v1/catalog")
+    fetch("/mocks/products.json")
         .then(res => res.json())
         .then(res => {
             if(res.error){
